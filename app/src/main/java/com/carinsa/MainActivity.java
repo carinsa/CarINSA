5652af80eed866005655dbfe0e682b33bdad5e7e
MainHanzo <33153091+MainHanzo@users.noreply.github.com>
2019-04-30 10:45:57 +0200

    Map view (#2)
    * mapView
    
    * marker (no realtime gps)

--- app/src/main/java/com/carinsa/MainActivity.java
+++ app/src/main/java/com/carinsa/MainActivity.java
@@ -1,18 +1,32 @@
 package com.carinsa;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.views.MapView;
+
+import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+
 
 public class MainActivity extends AppCompatActivity {
     MapView map = null;
+    MyItemizedOverlay myItemizedOverlay = null;
+    MyLocationNewOverlay myLocationOverlay = null;
     @Override public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         //handle permissions first, before map is created. not depicted here
 
         //load/initialize the osmdroid configuration, this can be done
@@ -25,10 +39,48 @@ public class MainActivity extends AppCompatActivity {
         //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's tile servers will get you banned based on this string
 
         //inflate and create the map
-        setContentView(R.layout.activity_main);
 
         map = (MapView) findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        IMapController mapController = map.getController();
+        mapController.setZoom(9.5);
+        GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
+        mapController.setCenter(startPoint);
+
+        //marker star
+        Drawable marker=getResources().getDrawable(android.R.drawable.star_big_on);
+        int markerWidth = marker.getIntrinsicWidth();
+        int markerHeight = marker.getIntrinsicHeight();
+        marker.setBounds(0, markerHeight, markerWidth, 0);
+
+        myItemizedOverlay = new MyItemizedOverlay(marker);
+        map.getOverlays().add(myItemizedOverlay);
+
+        GeoPoint myPoint1 = new GeoPoint(0*1000000, 0*1000000);
+        myItemizedOverlay.addItem(myPoint1, "myPoint1", "myPoint1");
+
+        //marker starter
+        Marker startMarker = new Marker(map);
+        startMarker.setPosition(startPoint);
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);// position the marker in center
+        map.getOverlays().add(startMarker);
+
+
+        myLocationOverlay = new MyLocationNewOverlay(map);
+        map.getOverlays().add(myLocationOverlay);
+        myLocationOverlay.enableMyLocation();
+
+//        Context mContext = getApplicationContext();
+//
+//        MapView mMap = (MapView) findViewById(R.id.mapview);
+//        mMap.setTileSource(TileSourceFactory.MAPNIK);
+//        //Add my location overlay
+//        MyLocationNewOverlay mMyLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(mContext), mMap);
+//        mMyLocationOverlay.enableMyLocation();
+//        mMap.getOverlays().add(mMyLocationOverlay);
     }
 
     public void onResume(){
@@ -38,6 +90,12 @@ public class MainActivity extends AppCompatActivity {
         //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
         map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+        GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
+        IMapController mapController = map.getController();
+        mapController.setZoom(9);
+        mapController.setCenter(startPoint);
+//add
+
     }
 
     public void onPause(){
@@ -48,4 +106,10 @@ public class MainActivity extends AppCompatActivity {
         //Configuration.getInstance().save(this, prefs);
         map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
     }
+//
+//    public View onCreateView() {
+//
+//        MapView mMapView = (MapView) findViewById(R.id.mapview);
+//        return mMapView;
+//    }
 }
