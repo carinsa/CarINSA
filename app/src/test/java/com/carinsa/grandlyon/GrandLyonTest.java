c7f62f0eae6db0c59c14ae64bc78cfa91a7eef0a
zaksah <zaksah2@hotmail.com>
2019-05-02 08:22:14 +0200

    création des test

---
+++ app/src/test/java/com/carinsa/grandlyon/GrandLyonTest.java
@@ -0,0 +1,28 @@
+package com.carinsa.grandlyon;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GrandLyonTest {
+
+    @Test
+    public void fetchParkings() {
+    }
+
+    @Test
+    public void fetchStatus() {
+    }
+
+    @Test
+    public void getAllParkings() {
+    }
+
+    @Test
+    public void getParkings() {
+    }
+
+    @Test
+    public void getClosestAvailableParking() {
+    }
+}
